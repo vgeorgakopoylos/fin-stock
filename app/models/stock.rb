e9e36273707267c2b419e3ab7c7b3d10ec61e2ff
vgeorgakopoylos <v.georgakopoylos@gmail.com>
2019-04-16 12:10:16 -0700

    Added the functionality to show stock info on search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,6 @@
 class Stock < ApplicationRecord
+  def self.new_from_lookup(ticker_symbol)
+    look_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    new(name: look_up_stock.company_name, ticker: look_up_stock.symbol, last_price: look_up_stock.latest_price)
+  end
 end
