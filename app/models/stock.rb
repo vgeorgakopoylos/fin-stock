680d3a3f59abb31590a94f23a0124b90eb1375a2
vgeorgakopoylos <v.georgakopoylos@gmail.com>
2019-04-16 12:19:51 -0700

    Added validations and exception handling on search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,10 @@
 class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
-    look_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    new(name: look_up_stock.company_name, ticker: look_up_stock.symbol, last_price: look_up_stock.latest_price)
+    begin
+      look_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      new(name: look_up_stock.company_name, ticker: look_up_stock.symbol, last_price: look_up_stock.latest_price)
+    rescue Exception => e
+      return nil
+    end
   end
 end
