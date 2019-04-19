d4a0488d5e4c85d7757ea8efa0f12a6cbe2d4c18
vgeorgakopoylos <v.georgakopoylos@gmail.com>
2019-04-19 01:09:59 -0700

    Added many to many associations between stock and user

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
   def self.new_from_lookup(ticker_symbol)
     begin
       look_up_stock = StockQuote::Stock.quote(ticker_symbol)
